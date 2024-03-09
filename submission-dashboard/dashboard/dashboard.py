25829e398aafc7745569d11be63c2573a2ff48ad
Bayu-DanuArtha <114640553+Bayu-DanuArtha@users.noreply.github.com>
2024-03-09 21:13:27 +0800

    Modify contents file dashboard.py

--- submission-dashboard/dashboard/dashboard.py
+++ submission-dashboard/dashboard/dashboard.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 from babel.numbers import format_currency
+import matplotlib.pyplot as plt
 sns.set(style='dark')
 
 st.markdown(
